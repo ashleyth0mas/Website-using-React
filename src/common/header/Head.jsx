f78cb537525ac7021be44621a7fe7739a5e6850e
ashleyth0mas <ashmariam@hotmail.com>
2022-11-26 22:01:19 +0530

    Second commit

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label>+1 1123 456 780 </label>
+            <i className='fa fa-envelope'></i>
+            <label> uilib.help@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+                     </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
